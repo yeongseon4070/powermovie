7f3d21335c9c74b9263fccfa18c93d396929d5b3
LEE <sunkind0709@gmail.com>
2021-12-07 20:35:46 +0900

    state, function test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./App.css"
-// {안에는 함수,상태값들이 들어간다.
+
 
 const App = () => {
+
+    // {안에는 함수,상태값들이 들어간다.
+    const [date, setDate] = useState("powermovie's second react App")
+    //자신이 이름을 정하고 변수를 입력.
+    const changeText = () => {
+        setDate("나의 세번째 리액트앱")
+    }
+    
   return (
       <div className={"App"}>
         <hi>
-          powermovie's first react App
+            {date}
         </hi>
-          <button>
+          <button onClick={changeText}>
               hi go!
           </button>
       </div>
